03d15445e9eb62c561eec894fce727cc5f8fe12c
dosaki <tiago.f.a.correia@gmail.com>
2014-11-01 19:35:35 +0000

    Trying pagination

--- tutorial/views.py
+++ tutorial/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from tutorial.models import Tutorial
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-    tutorials = Tutorial.objects.all()
-    return render(request, 'frontpage/index.html', {'tutorials':tutorials,})
+    max_obj = 25
+    page = 1
+
+    #KIND OF PAGINATION: NEEDS WORK!
+
+    #if (request.GET.get('max'))
+    #    max_obj = request.GET.get('max')
+    #if (request.GET.get('page'))
+    #    page = request.GET.get('page')
+
+    tutorials = Paginator(Tutorial.objects.all(), max_obj)
+    return render(request, 'frontpage/index.html', {'tutorials':tutorials.page(page),})
